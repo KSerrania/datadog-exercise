b93160e8752c1c6e5e34ec5993ffc95e3db79653
Kylian Serrania <kylian.serrania@student.ecp.fr>
2017-05-01 01:16:04 +0200

    Refactor(main): Messages are now sent together instead of website per website

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ from influxdb import InfluxDBClient
 from retriever import Retriever
 from monitor import Monitor
 
-def printResults(retriever, printInterval, minutes):
-    resultsPrinting = threading.Timer(printInterval, printResults, args=[retriever, 10, 10]) 
+def printResults(retrievers, printInterval, minutes):
+    resultsPrinting = threading.Timer(printInterval, printResults, args=[retrievers, printInterval, minutes])
     resultsPrinting.start()
-    availableStats, stats = retriever.getStats(minutes)
-    if availableStats:
-        alertStatus = retriever.checkAlert()
-        print('\n\033[94m--- Stats for website ' + retriever.URL + ' ----\033[0m' + \
-                '\nAverage ping latency over the last {} minutes: {:.2f} ms'.format(minutes, stats['avgLatency']) + \
-                '\nMaximum ping latency over the last {} minutes: {:.2f} ms'.format(minutes, stats['maxLatency']) + \
-                '\nMinimum ping latency over the last {} minutes: {:.2f} ms'.format(minutes, stats['minLatency']) + \
-                '\nPing response over the last {} minutes: {:.2%}'.format(minutes, stats['pingResponse']) + \
-                '\nSite availibility over the last {} minutes: {:.2%}'.format(minutes, stats['availability']) + \
-                alertStatus)
-        return 0;
-    else:
-        print('\n\033[93m--- No data available for website ' + retriever.URL + ' ----\033[0m')
+    totalString = str(datetime.now())
+    for website, retriever in retrievers.items():
+        availableStats, stats = retriever.getStats(minutes)
+        if availableStats:
+            alertStatus = retriever.checkAlert()
+            totalString += '\n\033[94m--- Stats for website ' + retriever.URL + ' ----\033[0m' + \
+                    '\nAverage ping latency over the last {} minutes: {:.2f} ms'.format(minutes, stats['avgLatency']) + \
+                    '\nMaximum ping latency over the last {} minutes: {:.2f} ms'.format(minutes, stats['maxLatency']) + \
+                    '\nMinimum ping latency over the last {} minutes: {:.2f} ms'.format(minutes, stats['minLatency']) + \
+                    '\nPing response over the last {} minutes: {:.2%}'.format(minutes, stats['pingResponse']) + \
+                    '\nSite availibility over the last {} minutes: {:.2%}'.format(minutes, stats['availability']) + \
+                    alertStatus + '\n'
+        else:
+            totalString += '\n\033[93m--- No data available for website ' + retriever.URL + ' ----\033[0m\n'
+    print(totalString)
+    return 0;
 
 def getResults(monitor, checkInterval):
     periodicCheck = threading.Timer(checkInterval, getResults, args=[monitor, checkInterval])
@@ -28,22 +31,22 @@ def getResults(monitor, checkInterval):
     monitor.get()
     return 0;
 
-def monitorWebsite(website, influxClient, checkInterval):
-    monitor = Monitor(website, influxClient)
-    retriever = Retriever(website, influxClient)
-    resultsPrinting10s = threading.Timer(10, printResults, args=[retriever, 10, 10])
-    resultsPrinting10s.start()
-    resultsPrinting1m = threading.Timer(60, printResults, args=[retriever, 60, 60])
-    resultsPrinting1m.start()
-    periodicCheck = threading.Timer(checkInterval, getResults, args=[monitor, checkInterval])
-    periodicCheck.start()
-
 def main(*websites, **kwargs):
     influxClient = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
     influxClient.create_database('example')
-    threads = {}
     checkInterval = kwargs['checkInterval']
+    monitors = {}
+    retrievers = {}
     for website in websites:
-        threads[website] = threading.Thread(target=monitorWebsite, name=website,args=[website, influxClient, checkInterval])
-        threads[website].daemon = True
-        threads[website].start()
+        monitors[website] = (Monitor(website, influxClient), checkInterval)
+        retrievers[website] = Retriever(website, influxClient)
+
+    resultsPrinting10s = threading.Timer(10, printResults, args=[retrievers, 10, 10])
+    resultsPrinting10s.start()
+    resultsPrinting1m = threading.Timer(60, printResults, args=[retrievers, 60, 60])
+    resultsPrinting1m.start()
+
+    for (monitor, checkI) in monitors.values():
+        periodicCheck = threading.Timer(checkI, getResults, args=[monitor, checkI])
+        periodicCheck.start()
+
