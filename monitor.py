0811b0fbde9c5cf8d67ea23f559a2e3de73adc18
Kylian Serrania <kylian.serrania@student.ecp.fr>
2017-05-07 16:46:05 +0200

    Docs(Monitor): Add docs and remove useless whitespace characters

--- monitor.py
+++ monitor.py
@@ -5,7 +5,7 @@ class Monitor():
     """Class whose goal is to check the monitored website's availability and performance.
 
     Attributes:
-        URL (str): URL of the monitored website.
+        URL (str): URL of the monitored website,
         influxClient (InfluxDBClient): Client for the InfluxDB used to store monitoring data.
 
     """
@@ -14,33 +14,33 @@ class Monitor():
         """Sets the URL and influxDBClient as speficied in the parameters.
 
         Args:
-            URL (str): URL of the monitored website.
+            URL (str): URL of the monitored website,
             influxClient (InfluxDBClient): Client for the InfluxDB used to store monitoring data.
 
         """
         self.URL = URL
         self.influxClient = influxClient
-        
+
     def __availabilityCheck(self):
         """Checks if the monitored website is available by sending it a GET request.
-        We define that a site is available if it responds to the GET request with a status which
-        doesn't start with 5.
+        We define that a site is available if it responds to a GET request with a status code which
+        doesn't start with 4 or 5.
 
         Returns:
             A tuple containing:
-                - a boolean (False if the site is not available, True if it is)
+                - a boolean (False if the site is not available, True if it is),
                 - a requests.Response object containing data about the requests, or None if the website
-                  didn't answer the request
+                  didn't answer the request.
         """
-        #TODO: Notify the user about 400 errors
 
         try:
-            # We send a request to the website
+            # Send a request to the website, and verify that it doesn't respond with an error code
             response = requests.get(self.URL)
             if response.status_code < 400:
                 return True, response
             else:
                 return False, response
+        # If the website doesn't respond, set that it is not available
         except requests.Timeout as e:
             #print('The request at {} timed out'.format(self.URL))
             return False, None
@@ -59,14 +59,23 @@ class Monitor():
         """Gets data about the monitored website and stores it into the InfluxDB database.
 
         """
+
+        # Get the current date to use it as a timestamp
         currentDate = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        # Query the website
         available, response = self.__availabilityCheck()
+
         if response is not None:
+            # If there was a response, get interesting fields about the response
             responseTime = response.elapsed.total_seconds() * 1000
             status = response.status_code
         else:
+            # If not, fill the blanks with None
             responseTime = None
             status = None
+
+        # Format data and write it to the database
         data = [
             {
                 "measurement": "website_availability",
